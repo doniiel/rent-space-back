4f2f677380e1a332ca7153dcac95a8823b4cedb3
don1yal <orynbekdanial8@gmail.com>
2024-11-22 16:01:37 +0600

    Listings service v1

--- listingservice/src/main/java/com/rentspace/listingservice/repository/PhotosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentspace.listingservice.repository;
-
-import com.rentspace.listingservice.entity.Photos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotosRepository extends JpaRepository<Photos, Long> {
-}
