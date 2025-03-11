857cffa5f3a67d19b83f372e887a37d603bfe6bc
Orynbek Daniyal <orynbekdanial8@gmail.com>
2025-03-11 16:30:14 +0500

    listing unblock block

---
+++ booking-service/src/main/java/com/rentspace/bookingservice/handler/BookingHandler.java
@@ -0,0 +1,59 @@
+package com.rentspace.bookingservice.handler;
+
+import com.rentspace.bookingservice.entity.Booking;
+import com.rentspace.bookingservice.enums.BookingStatus;
+import com.rentspace.bookingservice.exception.BookingNotFoundException;
+import com.rentspace.bookingservice.repository.BookingRepository;
+import com.rentspace.bookingservice.service.BookingService;
+import com.rentspace.core.enums.ListingStatus;
+import com.rentspace.core.event.BookingCreatedEvent;
+import com.rentspace.core.event.ListingAvailableResponse;
+import com.rentspace.core.event.PaymentFailureEvent;
+import com.rentspace.core.event.PaymentSuccessEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BookingConsumer {
+    private final BookingService service;
+    private final BookingRepository repository;
+
+    @KafkaListener(topics = "${event.topic.payment-success}", groupId = "${spring.kafka.consumer.group-id}")
+    public void handlerSuccessPaymentEvent(@Payload PaymentSuccessEvent event) {
+        log.info("Received payment success event for booking ID: {}", event.getBookingId());
+        service.updateBookingStatus(event.getBookingId(), BookingStatus.CONFIRMED);
+    }
+
+    @KafkaListener(topics = "${event.topic.payment-failure}", groupId = "${spring.kafka.consumer.group-id}")
+    public void handlerFailurePaymentEvent(@Payload PaymentFailureEvent event) {
+        log.info("Received payment failure event for booking ID: {}", event.getBookingId());
+        service.updateBookingStatus(event.getBookingId(), BookingStatus.CANCELLED);
+    }
+
+    @KafkaListener(topics = "${event.topic.listing-response}", groupId = "${spring.kafka.consumer.group-id}")
+    public void handlerListingAvailabilityEvent(@Payload ListingAvailableResponse event) {
+        log.info("Received listing availability response for booking ID: {}", event.getBookingId());
+
+        if (ListingStatus.AVAILABLE.equals(event.getStatus())) {
+            log.info("Listing is available. Proceeding with booking ID: {}", event.getBookingId());
+
+            Booking booking = repository.findById(event.getBookingId())
+                    .orElseThrow(() -> new BookingNotFoundException("Booking", "bookingId", event.getBookingId()));
+
+            BookingCreatedEvent event = BookingCreatedEvent.builder()
+                    .bookingId(booking.getId())
+                    .userId(booking.getUserId())
+                    .listingId(booking.getListingId())
+                    .totalPrice(booking.getTotalPrice())
+                    .build();
+            
+        }
+
+    }
+
+}
